79db87912d7389f5474c38f3ff8adf0c43358f15
Quan Nguyen <60857932+quan-ng03@users.noreply.github.com>
2023-12-01 19:40:52 -0700

    Add files via upload

--- memory_manager.c
+++ memory_manager.c
@@ -1,17 +1,7 @@
 //
 // Created by Aarya Reddy
 //
-#define VMA_ITERATOR(name, __mm, __addr)\
-	struct vma_iterator name = {    \
-		.mas = {                \
-		    .tree = __mm->mm_mt, \
-		    .index = __addr,      \
-		    .node = MAS_START,  \
-		},                      \
-	}
 
-#define for_each_vma(__vmi, __vma)  \
-while (((__vma) = vma_next(&(__vmi))) != NULL)
 
 #include <linux/kernel.h>
 #include <linux/hrtimer.h>
@@ -22,49 +12,31 @@ while (((__vma) = vma_next(&(__vmi))) != NULL)
 #include <linux/module.h>
 
 
+
 static int pid = 0;
 static struct task_struct* task = NULL;
 static int test_case;
 static struct hrtimer no_restart_hr_timer;
 
-module_param(pid, int, 0444);
+module_param(pid, int, 0);
 
 // Initialize memory statistics variables
 unsigned long total_rss = 0;
 unsigned long total_swap = 0;
 unsigned long total_wss = 0;
 
-
-int ptep_test_and_clear_young (struct vm_area_struct *vma, unsigned
-long addr, pte_t *ptep);
 /* Test and clear the accessed bit of a given pte entry. vma is the pointer
 to the memory region, addr is the address of the page, and ptep is a pointer
 to a pte. It returns 1 if the pte was accessed, or 0 if not accessed. */
 /* The ptep_test_and_clear_young() is architecture dependent and is not
 exported to be used in a kernel module. You will need to add its
 implementation as follows to your kernel module. */
-int ptep_test_and_clear_young(struct vm_area_struct *vma,
-unsigned long addr, pte_t *ptep)
+int ptep_test_and_clear_young(struct vm_area_struct *vma, unsigned long addr, pte_t *ptep)
 {
 	int ret = 0;
-	if (pte_young(*ptep)){
-		ret = test_and_clear_bit(_PAGE_BIT_ACCESSED, (unsigned long *) &ptep->pte);
-	}
+	if (pte_young(*ptep))
+		ret = test_and_clear_bit(_PAGE_BIT_ACCESSED,(unsigned long *) &ptep->pte);
 	return ret;
-}
-
-
-struct task_struct* get_task_struct_by_pid(int p)
-{
-	// loop iterator
-	for_each_process(task)
-	{
-		// if required task struct is found
-		if(task->p == p)
-			return task;
-	}
-
-	return NULL;
 }
 
 
@@ -76,8 +48,7 @@ static void parse_vma(void)
     struct mm_struct *mm = NULL;
 
     if(pid > 0){
- //       task = pid_task(find_vpid(pid), PIDTYPE_PID);
-        task = get_task_struct_by_pid(pid);
+        task = pid_task(find_vpid(pid), PIDTYPE_PID);
         if(task && task->mm){
             mm = task->mm;
             // TODO 2: mm_struct to initialize the VMA_ITERATOR (-- Assignment 4)
@@ -109,28 +80,28 @@ static void parse_vma(void)
                     pgd = pgd_offset(mm, vpage); // get pgd from mm and the page address
                     if (pgd_none(*pgd) || pgd_bad(*pgd)) {
                     // check if pgd is bad or does not exist
-                        return;
+                        continue;
                     }
                     
                     p4d = p4d_offset(pgd, vpage); //get p4d from from pgd and the page address
                         if (p4d_none(*p4d) || p4d_bad(*p4d)) {
                         // check if p4d is bad or does not exist
-                        return;
+                        continue;
                     }
                         
                     pud = pud_offset(p4d, vpage); // get pud from from p4d and the page address
                         if (pud_none(*pud) || pud_bad(*pud)) {
                         // check if pud is bad or does not exist
-                        return;
+                        continue;
                     }
 
                     pmd = pmd_offset(pud, vpage); // get pmd from from pud and the page address
                     if (pmd_none(*pmd) || pmd_bad(*pmd)) {
                         // check if pmd is bad or does not exist
-                        return;
+                        continue;
                     }
 
-                    ptep = pte_offset_map(pmd, vpage); // get pte from pmd and the page address
+                    ptep = pte_offset_kernel(pmd, vpage); // get pte from pmd and the page address
 
                     pte = *ptep;
                     
@@ -138,21 +109,24 @@ static void parse_vma(void)
                     // Hint: one if statement here
                         
                     if (pte_none(pte)){
-                    	return;
+                    	continue;
                     }
                     // TODO 7: use pte_present(pte) to check if the page is in memory, otherwise it is in swap
                     // TODO 8: use pte_young(pte) to check if the page is actively used
                     // if it is actively used, update wss and clear the accessed bit by: "test_and_clear_bit(_PAGE_BIT_ACCESSED,(unsigned long *)ppte);"
          
-		    if (pte_present(pte)) {
-		       	total_rss++;
-		        if (ptep_test_and_clear_young(vma,vpage,ptep)) {
-		           total_wss++;
-		         }
+		            if (pte_present(pte)) {
+		            	total_rss++;
+		                if (pte_young(pte)) {
+		                    total_wss++;
+		                    //ptep_test_and_clear_young(vma,vpage,ptep);
+		                    test_and_clear_bit(_PAGE_BIT_ACCESSED,(unsigned long *) &ptep->pte);
+
+		                }
 		                
-		    } else {
-		         total_swap++;
-		    }
+		            } else {
+		                total_swap++;
+		            }
                    
                 }
             } 
@@ -167,13 +141,13 @@ enum hrtimer_restart timer_callback( struct hrtimer *timer_for_restart )
 {
     ktime_t currtime , interval;
     currtime  = ktime_get();
-    interval = ktime_set(timer_interval_ns, 0);
+    interval = ktime_set(0,timer_interval_ns);
     hrtimer_forward(timer_for_restart, currtime , interval);
     total_rss = 0;
     total_swap = 0;
     total_wss = 0;
     parse_vma();
-    printk("[PID-%i]:[RSS:%lu MB] [Swap:%lu MB] [WSS:%lu MB]\n", pid, total_rss*4, total_swap*4, total_wss*4);
+    printk("[PID-%i]:[RSS:%lu KB] [Swap:%lu KB] [WSS:%lu KB]\n", pid, total_rss*4, total_swap*4, total_wss*4);
     return HRTIMER_RESTART;
 }
 
